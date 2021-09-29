4d212b231b6603f77698aee6e9d306310103ba35
Sergey Savelyev <sergey2304s@mail.ru>
2021-09-29 18:24:48 +0300

    check

--- src/ru/ev1n/untitled/Main.java
+++ src/ru/ev1n/untitled/Main.java
@@ -3,7 +3,11 @@ package ru.ev1n.untitled;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        System.out.println(Math.pow(2, 3));
+        System.out.println(Math.pow(2, 3));
+        System.out.println(Math.pow(2, 3));
+        System.out.println(Math.pow(2, 3));
         System.out.println(Math.pow(2, 3));
     }
 }
